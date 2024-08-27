d9093046ecdfc6005fc47f9d41598e10a28f2275
Abakar-Issa-Ali <abakar-issa-ali@laplateforme.io>
2024-08-27 10:10:46 +0200

    job2 done

---
+++ jour01/job02/multiply.cpp
@@ -0,0 +1,8 @@
+# include <iostream>
+using namespace std;
+int main(){
+    int number1 = 25; int number2 = 17;
+    int multiply = number1 * number2;
+    cout << "La multiplication de " <<number1<< " et " << number2 << " est " << multiply << endl;
+    return 0;
+}
